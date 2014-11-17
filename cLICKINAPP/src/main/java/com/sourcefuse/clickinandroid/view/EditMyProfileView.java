4a1090b89fdb9e0d2e17ccf9d3d7b7c732b0350c
Prafull Singh <prafull@prafull-singh.(none)>
2014-11-17 13:37:23 +0530

    resolve confilicts

--- cLICKINAPP/src/main/java/com/sourcefuse/clickinandroid/view/EditMyProfileView.java
+++ cLICKINAPP/src/main/java/com/sourcefuse/clickinandroid/view/EditMyProfileView.java
@@ -56,7 +56,7 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
       private String userName, userLastName, userEmail, userCity, userCountry;
       private Uri userImageUri;
 
-<<<<<<< HEAD
+
 
       @Override
       protected void onCreate(Bundle savedInstanceState) {
@@ -83,50 +83,7 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
 
 
             authManager = ModelManager.getInstance().getAuthorizationManager();
-=======
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.view_editprofile);
 
-        clickToSave = (Button) findViewById(R.id.btn_click_to_save);
-        myName = (EditText) findViewById(R.id.edt_my_name);
-        myLast = (EditText) findViewById(R.id.edt_my_last);
-        myEmail = (EditText) findViewById(R.id.edt_my_email);
-        myCity = (EditText) findViewById(R.id.edt_my_city);
-        myCountry = (EditText) findViewById(R.id.edt_my_country);
-        mySelfy = (ImageView) findViewById(R.id.iv_selfi);
-        backAction = (ImageView) findViewById(R.id.iv_menu);
-        OpenCamera = (ImageView) findViewById(R.id.iv_edit_camera);
-        OpenGallery = (ImageView) findViewById(R.id.iv_edit_gallery);
-
-        mySelfy.setScaleType(ImageView.ScaleType.FIT_XY);
-        clickToSave.setOnClickListener(this);
-        OpenCamera.setOnClickListener(this);
-        OpenGallery.setOnClickListener(this);
-        backAction.setOnClickListener(this);
-
-
-        authManager = ModelManager.getInstance().getAuthorizationManager();
-        try {
-            String[] names = (authManager.getUserName().split("\\s+", 2));
-            userName = names[0];
-            myName.setText("" + names[0]);
-            userLastName = names[1];
-            myLast.setText("" + names[1]);
-            userEmail = authManager.getEmailId();
-            myEmail.setText(userEmail);
-            userCity = authManager.getUserCity();
-            myCity.setText(userCity);
-            userCountry = authManager.getUserCountry();
-            myCountry.setText(userCountry);
-        } catch (Exception e) {
-        }
-
-        Uri tempUri = authManager.getUserImageUri();
-        if (tempUri != null) {
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
             try {
                   String[] names = (authManager.getUserName().split("\\s+", 2));
                   userName = names[0];
@@ -235,22 +192,7 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
             }
 
 //
-<<<<<<< HEAD
-      }
 
-      @Override
-      public void onClick(View v) {
-            switch (v.getId()) {
-                  case R.id.iv_edit_gallery:
-                        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        startActivityForResult(pickPhoto, Constants.SELECT_PICTURE);
-                        break;
-                  case R.id.iv_menu:
-                        finish();
-                        break;
-                  case R.id.iv_edit_camera:
-                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-=======
     }
 
     @Override
@@ -265,50 +207,13 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                 break;
             case R.id.iv_edit_camera:
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
+
 
                 mImageCaptureUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
                 intent.putExtra("return-data", true);
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
 
-<<<<<<< HEAD
-                        // start the image capture Intent
-                        startActivityForResult(intent, Constants.CAMERA_REQUEST);
-                        break;
-                  case R.id.btn_click_to_save:
-                        if (updateProfileValidation()) {
-                              if (Utils.isEmailValid(myEmail.getText().toString())) {
-                                    Log.e(TAG, "btn_click_to_save");
-                                    Utils.launchBarDialog(EditMyProfileView.this);
-                                    try {
-                                          authManager = ModelManager.getInstance().getAuthorizationManager();
-                                          profileManager = ModelManager.getInstance().getProfileManager();
-                                          userName = myName.getText().toString();
-                                          userLastName = myLast.getText().toString();
-                                          userEmail = myEmail.getText().toString();
-                                          userCity = myCity.getText().toString();
-                                          userCountry = myCountry.getText().toString();
 
-
-                                          ImageView im = (ImageView) findViewById(R.id.iv_selfi);
-                                          Bitmap bitmap = Bitmap.createBitmap(im.getWidth(), im.getHeight(), Bitmap.Config.ARGB_8888);
-                                          Canvas c = new Canvas(bitmap);
-                                          im.getDrawable().draw(c);
-                                          if (bitmap != null) {
-                                                profileManager.setProfile(userName, userLastName, authManager.getPhoneNo(),
-                                                                                 authManager.getUsrToken(), "", "", userCity, userCountry, userEmail, "", Utils.encodeTobase64(bitmap));
-                                          } else {
-                                                Log.e(TAG, "btn_click_to_save2");
-                                                try {
-                                                      // imageBitmap = Picasso.with(EditMyProfileView.this).load(authManager.getUserPic()).get();
-                                                      //Utils.encodeTobase64(imageBitmap)
-                                                      profileManager.setProfile(userName, userLastName, authManager.getPhoneNo(),
-                                                                                       authManager.getUsrToken(), "", "", userCity, userCountry, userEmail, "", "");
-                                                } catch (Exception e) {
-                                                      Log.e(TAG, "1" + e.toString());
-                                                }
-                                          }
-=======
                 // start the image capture Intent
                 startActivityForResult(intent, Constants.CAMERA_REQUEST);
                 break;
@@ -325,12 +230,13 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                             userEmail=myEmail.getText().toString();
                             userCity=myCity.getText().toString();
                             userCountry=myCountry.getText().toString();
-                            if (imageBitmap != null) {
 
-                                ImageView im = (ImageView) findViewById(R.id.iv_selfi);
-                                Bitmap bitmap = Bitmap.createBitmap(im.getWidth(), im.getHeight(), Bitmap.Config.ARGB_8888);
-                                Canvas c = new Canvas(bitmap);
-                                im.getDrawable().draw(c);
+                              ImageView im = (ImageView) findViewById(R.id.iv_selfi);
+                              Bitmap bitmap = Bitmap.createBitmap(im.getWidth(), im.getHeight(), Bitmap.Config.ARGB_8888);
+                              Canvas c = new Canvas(bitmap);
+                              im.getDrawable().draw(c);
+
+                            if (bitmap != null) {
 
                                 profileManager.setProfile(userName, userLastName, authManager.getPhoneNo(),
                                         authManager.getUsrToken(), "", "", userCity, userCountry, userEmail, "", Utils.encodeTobase64(bitmap));
@@ -345,7 +251,7 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                                     Log.e(TAG, "1" + e.toString());
                                 }
                             }
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
+
 
 
                                     } catch (Exception e) {
@@ -360,15 +266,11 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
             }
       }
 
-<<<<<<< HEAD
-      /* test code prafull */
-      public static final int MEDIA_TYPE_IMAGE = 1;
-      private static final String IMAGE_DIRECTORY_NAME = "FootGloryFlow Application";
-=======
+
     /* test code prafull */
     public static final int MEDIA_TYPE_IMAGE = 1;
     private static final String IMAGE_DIRECTORY_NAME = "FootGloryFlow Application";
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
+
 
     public static Uri getOutputMediaFileUri(int type) {
         return Uri.fromFile(getOutputMediaFile(type));
@@ -439,41 +341,13 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
             return true;
       }
 
-<<<<<<< HEAD
+
       @Override
       public void onActivityResult(int requestCode, int resultCode, Intent data) {
-=======
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) switch (requestCode) {
-            case Constants.CAMERA_REQUEST:
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
 
-<<<<<<< HEAD
             if (Constants.CAMERA_REQUEST == requestCode && resultCode == RESULT_OK) {
 
-=======
-                imageBitmap = BitmapFactory.decodeFile(mImageCaptureUri.getPath(), new BitmapFactory.Options());
-
-                try {
-                    ExifInterface ei = new ExifInterface(mImageCaptureUri.getPath());
-                    int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-
-                    int angle = 0;
-
-                    if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
-                        angle = 90;
-                    } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
-                        angle = 180;
-                    } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
-                        angle = 270;
-                    }
-                    Matrix mat = new Matrix();
-                    mat.postRotate(angle);
-                    Log.e("angle from camera 1 --->", "" + angle);
-                              /*bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), mat, true);*/
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
                   Bitmap bitmap = BitmapFactory.decodeFile(mImageCaptureUri.getPath(), new BitmapFactory.Options());
                   BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
@@ -484,7 +358,6 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                   bmpFactoryOptions.inJustDecodeBounds = false;
                   bitmap1 = BitmapFactory.decodeFile(mImageCaptureUri.getPath(), bmpFactoryOptions);
 
-<<<<<<< HEAD
                   try {
                         ExifInterface ei = new ExifInterface(mImageCaptureUri.getPath());
                         int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
@@ -522,64 +395,17 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                         // authManager.setUserPic(imageBitmap.toString());
                         mImageCaptureUri = null;
                         authManager.setMenuUserInfoFlag(true);
-=======
-                    Bitmap resized;
-                    if (imageBitmap.getWidth() >= imageBitmap.getHeight()) {
-
-                        resized = Bitmap.createBitmap(
-                                imageBitmap,
-                                imageBitmap.getWidth() / 2 - imageBitmap.getHeight() / 2,
-                                0,
-                                imageBitmap.getHeight(),
-                                imageBitmap.getHeight(), mat, true
-                        );
-
-                    } else {
-
-                        resized = Bitmap.createBitmap(
-                                imageBitmap,
-                                0,
-                                imageBitmap.getHeight() / 2 - imageBitmap.getWidth() / 2,
-                                imageBitmap.getWidth(),
-                                imageBitmap.getWidth(), mat, true
-                        );
-                    }
-                    imageBitmap.recycle();
-
-                    mySelfy.setImageBitmap(resized);
-                    authManager.setUserbitmap(resized);
-
-                    userImageUri = mImageCaptureUri;
-                    authManager.setUserImageUri(userImageUri);
-                    // authManager.setUserPic(imageBitmap.toString());
-                    mImageCaptureUri = null;
-                    authManager.setMenuUserInfoFlag(true);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-/*  test code prafull */
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
                   } catch (Exception e) {
                         e.printStackTrace();
                   }
 
-<<<<<<< HEAD
-=======
-                break;
-            case Constants.SELECT_PICTURE:
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
             }
             if (Constants.SELECT_PICTURE == requestCode && resultCode == RESULT_OK) {
 
 
-<<<<<<< HEAD
                   Bitmap bitmap = getBitmapFromCameraData(data, getApplicationContext());
-=======
-                imageBitmap = getBitmapFromCameraData(data, getApplicationContext());
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
 
                  /*    pick image from gallery  */
@@ -608,7 +434,6 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                         mat.postRotate(angle);
 
 
-<<<<<<< HEAD
                         Log.e("angle from gallery --->", "" + angle);
 
                         bitmap = Bitmap.createBitmap(bitmap1, 0, 0, bitmap1.getWidth(), bitmap1.getHeight(), mat, true);
@@ -617,26 +442,6 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                   } catch (Exception e) {
                         e.printStackTrace();
                   }
-=======
-                try {
-                    ExifInterface ei = new ExifInterface(getRealPathFromURI(data.getData()));
-                    int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-
-                    int angle = 0;
-
-                    if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
-                        angle = 90;
-                    } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
-                        angle = 180;
-                    } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
-                        angle = 270;
-                    }
-                    Matrix mat = new Matrix();
-                    mat.postRotate(angle);
-
-
-                    Log.e("angle from gallery --->", "" + angle);
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
 
 
@@ -665,7 +470,6 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                         );
                   }*/
 
-<<<<<<< HEAD
 
 
 
@@ -687,46 +491,9 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
                               Log.e("exception--->", "exception--->");
                         }
                   }
-=======
-                    Bitmap resized1;
-                    if (imageBitmap.getWidth() >= imageBitmap.getHeight()) {
-
-                        resized1 = Bitmap.createBitmap(
-                                imageBitmap,
-                                imageBitmap.getWidth() / 2 - imageBitmap.getHeight() / 2,
-                                0,
-                                imageBitmap.getHeight(),
-                                imageBitmap.getHeight(), mat, true
-                        );
-
-                    } else {
-
-                        resized1 = Bitmap.createBitmap(
-                                imageBitmap,
-                                0,
-                                imageBitmap.getHeight() / 2 - imageBitmap.getWidth() / 2,
-                                imageBitmap.getWidth(),
-                                imageBitmap.getWidth(), mat, true
-                        );
-                    }
-                    imageBitmap.recycle();
-
-                    mySelfy.setImageBitmap(resized1);
-                    authManager.setUserbitmap(resized1);
-                    userImageUri = data.getData();
-                    authManager.setUserImageUri(userImageUri);
-                    // authManager.setUserPic(imageBitmap.toString());
-
-                    authManager.setMenuUserInfoFlag(true);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
 
 
-<<<<<<< HEAD
                   /*authManager.setUserImageUri(userImageUri);*/
                   // authManager.setUserPic(imageBitmap.toString());
 
@@ -735,12 +502,6 @@ public class EditMyProfileView extends Activity implements View.OnClickListener 
 
             }
             if (Constants.CROP_PICTURE == requestCode && resultCode == RESULT_OK) {
-=======
-            default:
-                break;
-        }
-    }
->>>>>>> 2cedcf82e7488aa1c84e44e314a6cf164b46db88
 
                   if (data.getStringExtra("retake").equalsIgnoreCase("camare")) {
                         Intent intent1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
