b8d5b331635ba48438435e633fc9a86132318230
monika-bindal <monika.bindal@sourcefuse.com>
2015-02-02 10:30:50 +0530

    minor change

--- cLICKINAPP/src/main/java/com/sourcefuse/clickinandroid/services/ChatThread.java
+++ cLICKINAPP/src/main/java/com/sourcefuse/clickinandroid/services/ChatThread.java
@@ -333,7 +333,7 @@ public class ChatThread extends Thread implements QBMessageListener, ConnectionL
                         }
                         break;
                     case CHAT_LOGOUT:
-                        logoutQB();
+                    //    logoutQB();
                         break;
                     case CHECK_PRESENCE:
                         Bundle data1 = msg.getData();
